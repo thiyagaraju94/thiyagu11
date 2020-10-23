96ab910da8d1d8febf21b1ded2992fccfc5de5a2
thiyagaraju94 <thiyagarajutd94@gmail.com>
2020-10-23 13:23:29 +0530

    done all

---
+++ src/org/selenium3/InternetExplorer.java
@@ -0,0 +1,15 @@
+package org.selenium3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class InternetExplorer {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\SUDHAKAR K\\eclipse-workspace\\Selenium3\\Drivers\\IEDriverServer.exe");
+		
+		WebDriver driver = new InternetExplorerDriver();
+		
+		driver.get("https://www.facebook.com/");
+	}
+
+}
